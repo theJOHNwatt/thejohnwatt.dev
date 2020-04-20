11bc318ad122f049efa6dce86ea32d929bafb1a2
thejohnwatt <j.watt10@gmail.com>
2020-04-20 00:20:27 -0700

    components created and added to routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
+
+import Home from './components/home'
+import About from './components/about'
+import Tech from './components/tech'
+import Projects from './components/projects'
+import Contact from './components/contact'
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+
+
+      <Route path='/' render={_ => <Home/>} />
+      <Route path='/' render={_ => <About/>} />
+      <Route path='/' render={_ => <Tech/>} />
+      <Route path='/' render={_ => <Projects/>} />
+      <Route path='/' render={_ => <Contact/>} />
     </div>
   );
 }
---
+++ portfolio/src/components/tech.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tech() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tech;
